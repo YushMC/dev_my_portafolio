ef2e1a34cf69232a519b35c5d13c3485889da893
YushMC <mujushaxb10@gmail.com>
2025-03-27 00:42:50 -0600

    feat(SEO): add tags for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import vue from '@astrojs/vue';
+import vue from "@astrojs/vue";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), sitemap()]
+  site: "https://juanmendev.com",
+  integrations: [vue(), sitemap()],
 });
